5adee2383fc17977e59a45d73346b420922e3675
jmencl <jaksa.mencl@gmail.com>
2026-03-20 18:16:27 +0100

    Add files via upload

---
+++ Lab1/vjezba1.php
@@ -0,0 +1,23 @@
+<?php
+$naslov = "Prva vjezba iz PHP-a";
+$autor = "Jakša Mencl";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio je <strong>$autor</strong>.</p>";
+echo '<a href="https://www.w3schools.com/php/default.asp" target="_blank>"Posjeti W3Schools.com</a>';
+
+?>
+
+<!-- Naziv datoteke vjezba1.php -->
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prva vjezba</title>
+</head>
+<body>
+    
+</body>
+</html>
